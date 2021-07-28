2d40b0984b0c72e1d03a74448da024efdc0ceba5
JEmilyKoo <Jemilykoo@gmail.com>
2021-07-28 20:45:34 +0900

    .

--- app/src/main/java/kosmo/com/stampgo/HomeActivity.java
+++ app/src/main/java/kosmo/com/stampgo/HomeActivity.java
@@ -2,7 +2,11 @@ package kosmo.com.stampgo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -11,5 +15,20 @@ public class HomeActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
         //테스트!!
+
+
+        Button button1 =(Button) findViewById(R.id.button_home);
+        button1.setOnClickListener(new Button.OnClickListener(){
+
+            @Override
+            public void onClick(View v) {
+                Intent myIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://localhost:9090/exer/"));
+                startActivity(myIntent);
+            }
+
+            ;
+
+    });
+
     }
 }
